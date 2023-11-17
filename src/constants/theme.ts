df704bf25ff7b4484fa42c04414139927c25d8e8
sirbuland <sirbuland.atlas@gmail.com>
2023-11-17 05:51:05 -0500

    initial project setup with theme values

---
+++ src/constants/theme.ts
@@ -0,0 +1,15 @@
+import { colors } from "./colors";
+
+export const lightTheme = {
+  body: colors.white,
+  text: colors.black,
+  toggleBorder: colors.white,
+  background: colors.white,
+};
+
+export const darkTheme = {
+  body: colors.black,
+  text: colors.white,
+  toggleBorder: colors.black,
+  background: colors.black,
+};
